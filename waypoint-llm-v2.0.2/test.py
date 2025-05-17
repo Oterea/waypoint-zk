ca8f62803b4fbef6bab5630c504ed4dda61808b2
Oterea <hyczsin@gmail.com>
2025-05-17 14:55:09 +0800

    add

--- waypoint-llm-v2.0.2/test.py
+++ waypoint-llm-v2.0.2/test.py
@@ -14,18 +14,39 @@ def remote(prompt):
     response = requests.post(
         "http://192.168.2.62:11435/api/generate",
         json={
-            "model": "qwen2:0.5b",
+            "model": "qwen2:7b",
             "prompt": f"{prompt}",
             "stream": False
         }
     )
     print(response.json()["response"])
+
+
 def local():
     response = requests.post(
         "http://localhost:11434/api/generate",
         json={
-            "model": "qwen:0.5b",
+            "model": "qwen2:7b",
             "prompt": "你好？",
+            "stream": False
+        }
+    )
+    print(response.json())
+
+
+def remote_chat(prompt):
+
+
+    response = requests.post(
+        "http://192.168.2.62:11435/api/chat",
+        json={
+            "model": "qwen2:7b",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": f"{prompt}"
+                }
+            ],
             "stream": False
         }
     )
@@ -35,21 +56,8 @@ def local():
 if __name__ == '__main__':
     start = time.time()
     for i in range(10):
-        prompt = """
-        任务描述：你需要根据下面的信息输出格式为["目标高度", "目标速度", "目标航向",]的子目标。我有一个底层控制器可以接受这个子目标进而控制f16无人战斗机的飞行导航到目标点，同时避开障碍物。
-下面的信息包括无人机当前信息：当前三维坐标、当前速度、当前航向。终点信息：终点三维坐标、终点需要达到的速度。障碍物的三维坐标。
-坐标单位是米，速度单位是米每秒，航向单位是度(规定航向范围是(-180,180]，机头朝正y方向，航向为0；机头朝负y方向，航向为180；机头朝正x方向，航向为90；机头朝负x方向，航向为-90)
-"cur":{"cur_x":30000, "cur_y":0, "cur_z":6000, "cur_v":600, "cur_yaw": -90},
-"end":{"end_x":10000, "end_y":0, "end_z":7000, "end_v":900},
-"barrier":[{"b1_x":0, "b1_y":20000, "b1_z":7000}, {"b2_x":0, "b2_y":25000, "b2_z":6500}]
 
-你是一个无人机导航专家。我将提供的信息有：无人机信息：当前三维坐标位置和当前速度和当前航向；目标点信息：目标点三维坐标位置和到达目标点需要达到的速度。
+        prompt = """你是一个无人机导航专家。我将提供的信息有：无人机信息：当前三维坐标位置和当前速度和当前航向；目标点信息：目标点三维坐标位置和到达目标点需要达到的速度。
 你的任务是根据当前提供的信息对无人机进行全局的路径规划，你需要计算下一个子目标点T1的三维位置坐标和速度，直到到达终点。全局规划过程中的子目标点越少越好，不考虑障碍物。
-当前坐标x=30000，y=0，z=6000，当前速度600。终点坐标x=10000，y=0，z=7000，终点速度900。你需要输出子目标点的坐标序列和速度序列。
-        """
+当前坐标x=30000，y=0，z=6000，当前速度600。终点坐标x=10000，y=0，z=7000，终点速度900。你需要输出子目标点的坐标序列和速度序列。"""
         remote(prompt)
-
-
-
-
-
